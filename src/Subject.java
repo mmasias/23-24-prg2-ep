78b68c3224795b2827cd048bbfe5f35370cac327
eirik.rosete <eirik.rosete@gmail.com>
2024-04-13 01:04:30 +0200

    Creacion de la clase Subject
    Se creo la clase Subject, encargada de manejar todo lo relativo a la asignatura, en este caso esta encargada de ser impartida por un profesor (Clase Teacher) y de asignar un examen, clase que se creara mas tarde. Se edito la indentacion y los espaciados en las clases creadas.

---
+++ src/Subject.java
@@ -0,0 +1,54 @@
+class Subject {
+
+    private String name;
+    private String code;
+    private int credits;
+    private Teacher teacher;
+    private Test test;
+
+    public Subject(String name, String code, int credits){
+        assert name!=null && code.length()==4 && credits>=0;
+
+        this.name = name;
+        this.code = code;
+        this.credits = credits;
+    }
+    public Subject(String name, String code){
+        this(name, code, 0);
+    }
+    public Subject(String name){
+        this(name, name.substring(0,3).toUpperCase(), 0);
+    }
+    public Subject(){
+        this("Subject", "EDIT", 0);
+    }
+    public void editName(String name){
+        assert name != null;
+
+        this.name = name;
+    }
+    public void editCode(String code){
+        assert code.length()==4;
+
+        this.code = code.toUpperCase();
+    }
+    public void editCredits(int credits){
+        assert credits>=0;
+
+        this.credits= credits;
+    }
+    public Teacher addTeacher(Teacher teacher){
+        assert teacher != null;
+
+        this.teacher = teacher;
+        return teacher;
+    }
+    public Test addTest(Test test){
+        assert test != null;
+        this.test = test;
+        return test;
+    }
+    public String show(){
+        return "Asignatura: " + name + " - " + code + " - " + credits + " creditos";
+    }
+}
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,31 +1,36 @@
-class Teacher{
+class Teacher {
 
     private String name;
     private String id;
 
-    public Teacher(String name, String id){
-        assert name!=null && id!=null;
+    public Teacher(String name, String id) {
+        assert name != null && id != null;
 
         this.name = name;
         this.id = id;
     }
-    public Teacher(String name){
-        this(name, name.substring(0,2) + " XXXXXXX");
+
+    public Teacher(String name) {
+        this(name, name.substring(0, 3) + " XXXXXXX");
     }
-    public Teacher(){
+
+    public Teacher() {
         this("Teacher", "Ids missing");
     }
-    public void editName(String name){
-        assert name!=null;
+
+    public void editName(String name) {
+        assert name != null;
 
         this.name = name;
     }
-    public void editId(String id){
-        assert id!=null;
+
+    public void editId(String id) {
+        assert id != null;
 
         this.id = id;
     }
-    public String show(){
-        return "Nombre del profesor: " + name +  " - " + id;
+
+    public String show() {
+        return "Nombre del profesor: " + name + " - " + id;
     }
 }
