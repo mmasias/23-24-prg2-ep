52d3b626dfa8a976a1735295810e4ed3e95b1b32
eirik.rosete <eirik.rosete@gmail.com>
2024-04-13 01:04:42 +0200

    Creacion de la clase Subject
    Se creo la clase Subject, encargada de manejar todo lo relativo a la asignatura, en este caso esta encargada de ser impartida por un profesor (Clase Teacher) y de asignar un examen, clase que se creara mas tarde. Se edito la indentacion y los espaciados en las clases creadas.

--- src/Subject.java
+++ src/Subject.java
@@ -6,49 +6,58 @@ class Subject {
     private Teacher teacher;
     private Test test;
 
-    public Subject(String name, String code, int credits){
-        assert name!=null && code.length()==4 && credits>=0;
+    public Subject(String name, String code, int credits) {
+        assert name != null && code.length() == 4 && credits >= 0;
 
         this.name = name;
         this.code = code;
         this.credits = credits;
     }
-    public Subject(String name, String code){
+
+    public Subject(String name, String code) {
         this(name, code, 0);
     }
-    public Subject(String name){
-        this(name, name.substring(0,3).toUpperCase(), 0);
+
+    public Subject(String name) {
+        this(name, name.substring(0, 3).toUpperCase(), 0);
     }
-    public Subject(){
+
+    public Subject() {
         this("Subject", "EDIT", 0);
     }
-    public void editName(String name){
+
+    public void editName(String name) {
         assert name != null;
 
         this.name = name;
     }
-    public void editCode(String code){
-        assert code.length()==4;
+
+    public void editCode(String code) {
+        assert code.length() == 4;
 
         this.code = code.toUpperCase();
     }
-    public void editCredits(int credits){
-        assert credits>=0;
 
-        this.credits= credits;
+    public void editCredits(int credits) {
+        assert credits >= 0;
+
+        this.credits = credits;
     }
-    public Teacher addTeacher(Teacher teacher){
+
+    public Teacher addTeacher(Teacher teacher) {
         assert teacher != null;
 
         this.teacher = teacher;
         return teacher;
     }
-    public Test addTest(Test test){
+
+    public Test addTest(Test test) {
         assert test != null;
         this.test = test;
         return test;
     }
-    public String show(){
+
+    public String show() {
         return "Asignatura: " + name + " - " + code + " - " + credits + " creditos";
     }
 }
