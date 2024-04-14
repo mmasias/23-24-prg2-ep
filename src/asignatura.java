895b821553ccb855cb2954593cfb310496f95aba
DylanNRJ <dylannaranjo53@gmail.com>
2024-04-14 17:09:50 +0200

    Agregando mis clases y objetos publicos como privados para la entrega de mi Examen.

---
+++ src/asignatura.java
@@ -0,0 +1,20 @@
+public class asignatura {
+    private String nameasignatura;
+    private String id;
+    private int credits;
+
+    public asignatura(String nameasignatura, String id, int credits) {
+        this.nameasignatura = nameasignatura;
+        this.id = id;
+        this.credits = credits;
+    }
+
+    public void mostrarasignaturas() {
+        System.out.println("Asignatura: " + nameasignatura + " - " + id + " - " + credits + " créditos");
+    }
+
+    public static void main(String[] args) {
+        asignatura asignatura = new asignatura("Programación 2", "PRG2", 6);
+        asignatura.mostrarasignaturas();
+    }
+}
---
+++ src/examen.java
@@ -0,0 +1,29 @@
+public class examen {
+    private String nameExamen;
+    private asignatura asignatura;
+    private profesores profesor;
+    private String preguntas;
+
+    public examen(String nameExamen, asignatura asignatura, profesores profesor, String preguntas) {
+        this.nameExamen = nameExamen;
+        this.asignatura = asignatura;
+        this.profesor = profesor;
+        this.preguntas = preguntas;
+    }
+
+    public void mostrarExamen() {
+        profesor.mostrarProfesor();
+        asignatura.mostrarasignaturas();
+        System.out.println("Examen: " + nameExamen + " / Vigilado por Profᵃ.: Loyda Alas - DNI 433452A");
+        System.out.println("Preguntas:");
+        System.out.println(preguntas);
+    }
+
+    public static void main(String[] args) {
+        profesores profesor = new profesores("Manuel Masias", "666999XY");
+        asignatura asignatura = new asignatura("Programacion 2", "PRG2", 6);
+        examen examen = new examen("Examen Parcial", asignatura, profesor,
+                "1. Vista pública clases\n2. Vista pública de objetos\n3. Vista privada de clases & objetos");
+        examen.mostrarExamen();
+    }
+}
