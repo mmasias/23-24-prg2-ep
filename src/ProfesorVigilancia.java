a284d22d0388df5f110138679cd873e7fcdc7f81
Sergiorl2 <sergio.rodriguez1@alumnos.uneatlantico.es>
2024-04-15 23:05:39 +0200

    Vista privada ProfesorVigilancia

--- src/ProfesorVigilancia.java
+++ src/ProfesorVigilancia.java
@@ -1,4 +1,12 @@
 public class ProfesorVigilancia{
-    public ProfesorVigilancia(String nombre, String NIE) {}   
-    public ProfesorVigilancia cambiarProfesorVigilancia(ProfesorVigilancia profesorVigilancia) {}
+    private String nombre;
+    private String NIE;
+
+    public ProfesorVigilancia(String nombre, String NIE) {
+        this.nombre = nombre;
+        this.NIE = NIE;
+    }   
+    public ProfesorVigilancia cambiarProfesorVigilancia(ProfesorVigilancia profesorVigilancia) {
+        return profesorVigilancia;
+    }
 }
