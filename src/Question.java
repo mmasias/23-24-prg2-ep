8412df0a045a9baba4de85004eb786d56f9ec0ff
enmabry <bryanbaquedano11@gmail.com>
2024-04-15 20:07:24 +0200

    Implementé un public static void main para comprobar mis métodos, y todos funcionan correctamente, hasta ahora no he hecho ningún cambio significativo referente a mi modelado.

--- src/Question.java
+++ src/Question.java
@@ -21,8 +21,6 @@ public class Question{
             for (int i = 0; i < options.length; i++) {
                 System.out.println((i + 1) + ". " + options[i]);
             }
-        } else {
-            System.out.println("No hay opciones disponibles.");
         }
     }
 
@@ -32,6 +30,25 @@ public class Question{
     }
 
 
-    public void editOptions(String question, String[] options){}
+    public void editOptions(String newQuestion, String[] newOptions) {
+        String[] tempOptions = newOptions;
+        this.question = newQuestion;
+        this.options = tempOptions;
+    }
+
+    public static void main(String[] args) {
+
+        Question pregunta1 = new Question("¿Que opinas de la muerte?");
+        pregunta1.show();
+        
+        Question pregunta2 = new Question("¿Quien eres?");
+        pregunta2.show();
+        pregunta2.edit("¿Quien quieres llegar a ser?");
+        pregunta2.show();
+
+        Question pregunta3 = new Question("¿Quién escribió Humano, Demasiado Humano", new String[]{"Nietzsche", "Descartes", "Shoppenhauer", "Kant"});
+        pregunta3.editOptions("¿Quién escribió Humano, Demasiado Humano", new String[]{"Nietzsche", "Hume", "Shoppenhauer", "Kant"});
+        pregunta3.show();
+    }
 
 }
