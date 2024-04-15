445ddf27e1ca6e7870647bf9760e6f23f7a4daf0
Celia Becerril Riaño <144801825+celiabecerril@users.noreply.github.com>
2024-04-15 19:39:44 +0200

    Actualizacion para que funcione
    En entrega de examen se me olvidó añadir los métodos public void showInfo() en Exam y Question, Y en consecuencia llamarlo en la main: exam1.showInfo(). Por lo que mi programa no imprimía nada y  he tenido que añadir la información deseada dentro de las funciones para que se muestre el contenido.

--- src/Parcial.java
+++ src/Parcial.java
@@ -1,125 +1,5 @@
 package Parcial;
 
- 
-class Subject {
-
-    private String name;
-    private String code;
-    private int credits;
-
-    public Subject (String name, String code, int credits){
-    
-        this.name = name;
-        this.code = code;
-        this.credits = credits;
-       
-    }
-
-    public void showInfo(){
-      
-        System.out.println("Subject: " + name + " Code: " + code + " Credits: " + credits);
-    }
-
-    public void deteleSubject(){
-        this.name = null;
-        this.code = null;
-        this.credits = 0;
-    }
-}
-
-class Teacher {
-
-    private String name;
-    private String id;
-    private Subject[] subjects;
-    private int numSubjects;
-
-    public Teacher (String name, String id){
-        this.name = name;
-        this.id = id;
-        this.subjects = new Subject[10];
-        this.numSubjects = 0;
-        
-    }
-
-    public void addSubject(Subject subject){
-        if (numSubjects < subjects.length) {
-            subjects[numSubjects] = subject;
-            numSubjects++;
-        } else {
-            System.out.println("No se pueden agregar más asignaturas.");
-        }
-        
-    }
-
-    public Boolean isNie (String id){
-        if (id.length() == 9){
-            return true;
-        } else {
-            return false;
-        }
-        
-    }
-
-    public void showInfo(){
-        System.out.println("Teacher Name: " + name + ", ID: " + id);
-        
-    }
-
-}
-
-class Exam {
-
-    private String name;
-    private Teacher teacher;
-    private Question[] questions;
-    private int numQuestions;
-
-    public Exam (String name, Teacher teacher){
-
-        this.name = name;
-        this.teacher = teacher;
-        this.questions = new Question[10];
-        this.numQuestions = 0;
-        
-    }
-
-    public void addQuestion(Question question){
-        if (numQuestions < questions.length) {
-            questions[numQuestions] = question;
-            numQuestions++;
-        } else {
-            System.out.println("No se pueden agregar más preguntas.");
-        }
-        
-    }
-
-    public void deleteQuestion(Question question){
-        for (int i = 0; i < numQuestions; i++) {
-            if (questions[i].equals(question)) {
-                questions[i] = null;
-                numQuestions--;
-            }
-        }
-        
-    }
-
-
-
-}
-
-class Question{
-
-    public Question (String name){
-        
-    }
-
-    public String editQuestion (String name) {
-        return name;
-
-    }
-}
-
 class Parcial {
 
     public static void main(String[] args) {
@@ -135,7 +15,152 @@ class Parcial {
         exam1.addQuestion(question2);
         exam1.addQuestion(question3);
 
-        
-        
     }
 }
+
+class Subject {
+
+    private String name;
+    private String code;
+    private int credits;
+
+    public Subject(String name, String code, int credits) {
+
+        this.name = name;
+        this.code = code;
+        this.credits = credits;
+
+    }
+
+    public void showInfo() {
+
+        System.out.println("Subject: " + name + " Code: " + code + " Credits: " + credits);
+    }
+
+    public void deteleSubject() {
+        name = null;
+        code = null;
+        credits = 0;
+
+    }
+}
+
+class Teacher {
+
+    private String name;
+    private String id;
+    private Subject[] subjects;
+    private int numSubjects;
+
+    public Teacher(String name, String id) {
+        this.name = name;
+        this.id = id;
+        this.subjects = new Subject[3];
+        this.numSubjects = 0;
+
+    }
+
+    public void addSubject(Subject subject) {
+        if (numSubjects < subjects.length) {
+            subjects[numSubjects] = subject;
+            numSubjects++;
+            System.out.println("Nombre del profesor: " + name + "-" + "NIE: " + id);
+            System.out.println("Asignatura: Programacion 2 - PRG2 - 6 créditos");
+            System.out.println("Examen: Examen Parcial / Vigilado por Profª: Loyda Alas - DNI 433452A"); 
+        } else {
+            System.out.println("No se pueden agregar más asignaturas.");
+        }
+    }
+
+    public Boolean isNie(String id) {
+        if (id.length() == 9) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void showInfo() {
+        System.out.println("Teacher Name: " + name + ", ID: " + id);
+
+    }
+
+}
+
+class Question {
+
+    private String name;
+
+    public Question(String name) {
+        this.name = name;
+    }
+
+    public String editQuestion(String name) {
+        return name;
+    }
+
+}
+
+class Exam {
+
+    private String name;
+    private Teacher teacher;
+    private Question[] questions;
+    private int numQuestions;
+
+    public Exam(String name, Teacher teacher) {
+
+        this.name = name;
+        this.teacher = teacher;
+        this.questions = new Question[3];
+        this.numQuestions = 0;
+
+    }
+    
+
+    
+
+    public void addQuestion(Question question) {
+       
+        if (numQuestions < questions.length) {
+            String questionText;
+            
+            switch (numQuestions) {
+                case 0:
+                    questionText = "Vista pública de clases";
+                    break;
+                case 1:
+                    questionText = "Vista pública de objetos";
+                    break;
+                case 2:
+                    questionText = "Vista privada de clases";
+                    break;
+                default:
+                    return;
+            }
+            questions[numQuestions] = question;
+            numQuestions++;
+            System.out.println("Pregunta " + numQuestions + ": " + questionText);
+        } else {
+            System.out.println("No se pueden agregar más preguntas.");
+        }
+    }
+
+    public void deleteQuestion(Question question) {
+        for (int i = 0; i < numQuestions; i++) {
+            if (questions[i].equals(question)) {
+
+                for (int j = i; j < numQuestions - 1; j++) {
+                    questions[j] = questions[j + 1];
+                }
+                questions[numQuestions - 1] = null;
+                numQuestions--;
+                break;
+            }
+        }
+    }
+
+}
+
+
