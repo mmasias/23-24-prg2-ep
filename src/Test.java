5da83b9b494b8673fddd63413282bc8fc4134f17
enmabry <bryanbaquedano11@gmail.com>
2024-04-15 22:43:51 +0200

    Método para asociarle preguntas a mi Examen
    Este fue el método que mas trabajo me dio, al principio había hecho un método que solo me aceptaba una única pregunta, entonces tuve que implementar un arreglo de preguntas. Cree unos atributos para poder ver el numero actual de preguntas, y el total de preguntas que se puede hacer. Y listo al asociar 3 preguntas y darle show, se mostrarán las 3 preguntas. Todo esto lo comprobaré através de un método estático

--- src/Test.java
+++ src/Test.java
@@ -3,19 +3,31 @@ public class Test{
     private String type;
     private String percentageScore;
     private Teacher teacher;
+    private Question[] questions; 
+    private int numQuestions; 
 
     public Test( String type, String percentageScore){
         this.type = type;
         this.percentageScore = percentageScore;
+        this.questions = new Question[3]; 
+        this.numQuestions = 0;
     }
     public Test( String type, String percentageScore, Teacher teacher){
         this.type = type;
         this.percentageScore = percentageScore;
         this.teacher = teacher;
+        this.questions = new Question[3]; 
+        this.numQuestions = 0;
     }
 
     public void show(){
         System.out.println("Examen: " + type + "- Valor en la nota final: " + percentageScore + " / Vigilado por Prof." + teacher);
+        System.out.println("Preguntas: ");
+        System.out.println("Preguntas: ");
+        for (int i = 0; i < numQuestions; i++) {
+            System.out.print((i + 1) + ". ");
+            questions[i].show();
+        }
     }
     public void editPercentageScore(String newPercentageScore){
         String tempScore = newPercentageScore;
@@ -25,5 +37,10 @@ public class Test{
         Teacher tempTeacher = newTeacher;
         this.teacher = tempTeacher;
     }
-    public void addQuestion(Question question){}
+    public void addQuestion(Question question){
+        if (numQuestions < questions.length) {
+            questions[numQuestions] = question; 
+            numQuestions++; 
+        } 
+    }
 }
