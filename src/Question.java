049068e03e7f8eac52ebdffc6586220391457192
enmabry <bryanbaquedano11@gmail.com>
2024-04-15 19:48:36 +0200

    Implementación Clase Pregunta
    Por el momento he definido los atributos que serían la pregunta como tal, y en caso de ser de opción múltiple, sus respectivas opciones. Además cree los constructores, y implementé el método de show que muestra nuestra pregunta y sus opciones, y el método editar que con una variable temporal edita le asigna un nuevo valor a el atributo.

---
+++ src/Question.java
@@ -0,0 +1,38 @@
+public class Question{
+
+    private String question;
+    private String[] options;
+
+    public Question(String question){
+        this.question = question;
+        this.options = new String[0];
+    }
+
+    public Question(String question, String[] options){
+        this.question = question;
+        this.options = options;
+    }
+
+    public void show() {
+        System.out.println("Pregunta: " + question);
+        
+        if (options.length > 0) {
+            System.out.println("Opciones:");
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ". " + options[i]);
+            }
+        } else {
+            System.out.println("No hay opciones disponibles.");
+        }
+    }
+
+    public void edit(String newQuestion) {
+        
+        String tempQuestion = newQuestion;
+        this.question = tempQuestion;
+    }
+
+    
+    public void editOptions(String question, String[] options){}
+
+}
