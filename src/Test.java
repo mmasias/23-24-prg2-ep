060f5072d84495ccbd35f2bc0db073659fdfb900
enmabry <bryanbaquedano11@gmail.com>
2024-04-15 22:34:49 +0200

    Implementación de constructores clase Examen y métodos de edición.
    Implementé los constructores de la manera mas tradicional posible, y los métodos de edición que al parecer en mi caso se ha vuelto costumbre el usar variables temporales.

--- src/Test.java
+++ src/Test.java
@@ -4,13 +4,26 @@ public class Test{
     private String percentageScore;
     private Teacher teacher;
 
-    public Test( String type, String percentageScore){}
-    public Test( String type, String percentageScore, Teacher teacher){}
+    public Test( String type, String percentageScore){
+        this.type = type;
+        this.percentageScore = percentageScore;
+    }
+    public Test( String type, String percentageScore, Teacher teacher){
+        this.type = type;
+        this.percentageScore = percentageScore;
+        this.teacher = teacher;
+    }
 
     public void show(){
-        System.out.println("Examen: " + type + "- Valor en la nota final: " + percentageScore + " / " + teacher);
+        System.out.println("Examen: " + type + "- Valor en la nota final: " + percentageScore + " / Vigilado por Prof." + teacher);
     }
-    public void editPercentageScore(String percentageScore){}
-    public void editTeacher(Teacher teacher){}
+    public void editPercentageScore(String newPercentageScore){
+        String tempScore = newPercentageScore;
+        this.percentageScore = tempScore;
+    }
+    public void editTeacher(Teacher newTeacher){
+        Teacher tempTeacher = newTeacher;
+        this.teacher = tempTeacher;
+    }
     public void addQuestion(Question question){}
 }
