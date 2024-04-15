828d884c52c73ae938232baaa72dd97660ea11eb
Ian Saucedo <s.ians2212@gmail.com>
2024-04-16 00:16:53 +0200

    Clase pregunta

---
+++ src/Pregunta.java
@@ -0,0 +1,18 @@
+public class Pregunta {
+private String titulo;
+private String enunciado;
+
+Pregunta(String titulo, String enunciado){
+this.titulo= titulo;
+this.enunciado=enunciado;
+}
+
+Public String modificarTitulo(String titulo){
+this.titulo= titulo;
+}
+
+Public String modificarEnunciado(String enunciado){
+this.enunciado= enunciado;
+    }
+
+}
