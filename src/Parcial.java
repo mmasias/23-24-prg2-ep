dac49c033854360902c2e17b4ec18c3e288dfbef
Celia Becerril Riaño <144801825+celiabecerril@users.noreply.github.com>
2024-04-15 17:58:10 +0200

    vista privada (1)

--- src/Parcial.java
+++ src/Parcial.java
@@ -3,34 +3,66 @@ package Parcial;
  
 class Subject {
 
+    private String name;
+    private String code;
+    private int credits;
+
     public Subject (String name, String code, int credits){
+    
+        this.name = name;
+        this.code = code;
+        this.credits = credits;
        
     }
 
     public void showInfo(){
       
+        System.out.println("Subject: " + name + " Code: " + code + " Credits: " + credits);
     }
 
     public void deteleSubject(){
-        
+        this.name = null;
+        this.code = null;
+        this.credits = 0;
     }
 }
 
 class Teacher {
 
+    private String name;
+    private String id;
+    private Subject[] subjects;
+    private int numSubjects;
+
     public Teacher (String name, String id){
+        this.name = name;
+        this.id = id;
+        this.subjects = new Subject[10];
+        this.numSubjects = 0;
         
     }
 
     public void addSubject(Subject subject){
+        if (numSubjects < subjects.length) {
+            subjects[numSubjects] = subject;
+            numSubjects++;
+        } else {
+            System.out.println("No se pueden agregar más asignaturas.");
+        }
         
     }
 
     public Boolean isNie (String id){
+        if (id.length() == 9){
+            return true;
+        } else {
+            return false;
+        }
         
     }
 
     public void showInfo(){
+        System.out.println("Teacher Name: " + name + ", ID: " + id);
         
     }
 
@@ -38,16 +70,37 @@ class Teacher {
 
 class Exam {
 
+    private String name;
+    private Teacher teacher;
+    private Question[] questions;
+    private int numQuestions;
 
     public Exam (String name, Teacher teacher){
+
+        this.name = name;
+        this.teacher = teacher;
+        this.questions = new Question[10];
+        this.numQuestions = 0;
         
     }
 
     public void addQuestion(Question question){
+        if (numQuestions < questions.length) {
+            questions[numQuestions] = question;
+            numQuestions++;
+        } else {
+            System.out.println("No se pueden agregar más preguntas.");
+        }
         
     }
 
     public void deleteQuestion(Question question){
+        for (int i = 0; i < numQuestions; i++) {
+            if (questions[i].equals(question)) {
+                questions[i] = null;
+                numQuestions--;
+            }
+        }
         
     }
 
@@ -61,8 +114,8 @@ class Question{
         
     }
 
-     
     public String editQuestion (String name) {
+        return name;
 
     }
 }
