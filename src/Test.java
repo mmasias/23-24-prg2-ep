c0d3ffdbcdf33c90125300429104806182710313
enmabry <bryanbaquedano11@gmail.com>
2024-04-15 22:30:40 +0200

    Implemenatación atributos y el método ver
    Implementé los atributos de mi clase, y el método ver, que reflejará el tipo de examen, valor de nota final y  el maestro que vigila.

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,15 @@
 public class Test{
 
+    private String type;
+    private String percentageScore;
+    private Teacher teacher;
+
     public Test( String type, String percentageScore){}
     public Test( String type, String percentageScore, Teacher teacher){}
 
-    public void show(){}
+    public void show(){
+        System.out.println("Examen: " + type + "- Valor en la nota final: " + percentageScore + " / " + teacher);
+    }
     public void editPercentageScore(String percentageScore){}
     public void editTeacher(Teacher teacher){}
     public void addQuestion(Question question){}
