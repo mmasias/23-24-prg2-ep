689a51ecd1d041affde43216a2ba1e834442b7c7
enmabry <bryanbaquedano11@gmail.com>
2024-04-15 20:54:07 +0200

    Implementación constructores de la clase Maestro
    Implementé los constructores y creé los atributos.

---
+++ src/Teacher.java
@@ -0,0 +1,18 @@
+public class Teacher {
+    
+    private String name;
+    private String id;
+
+    public Teacher(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+    public Teacher(String name){
+        this(name, "");
+    }
+
+    public void show(){}
+    public void editName(String name){}
+    public void editId(String id){}
+    public void addSubject(Subject subject){}
+}
