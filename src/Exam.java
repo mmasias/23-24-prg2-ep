b04b7ed25ab1ae1953e19f67576b36ff6d82ac7e
Camila <clpalma2003@gmail.com>
2024-04-16 00:40:52 +0200

    feat:Add the Exam class

---
+++ src/Exam.java
@@ -0,0 +1,30 @@
+public class Exam {
+    private String header;
+    private String[] questions;
+
+    public Exam(String header) {
+        this.header = header;
+        this.questions = new String[3];
+    }
+
+    public String header() {
+        return this.header;
+    }
+
+    public Question[] add(int number, String question) {
+        questions[number - 1] = question;
+        return null;
+    }
+
+    public int number() {
+        return this.questions.length;
+    }
+
+    public String question() {
+        return this.questions.toString();
+    }
+
+    public Professor responsable() {
+        return null;
+    }
+}
