f401b9dbc51cd6f933230b783a70868c20e09abc
enmabry <bryanbaquedano11@gmail.com>
2024-04-15 20:18:55 +0200

    Creación de clase Asignatura
    He empezado a copiar todo lo que tenía en mi modelado, y hice los constructores y los métodos, a continuación empezaré con la implementación.

---
+++ src/Subject.java
@@ -0,0 +1,15 @@
+public class Subject {
+    
+    public Subject(String name, String code, int credits){}
+    public Subject(String name, String code){}
+    public Subject(String name, int credits){}
+    public Subject(String name){}
+
+    public void show(){}
+    public String createCode(String name){
+        return "a";
+    }
+    public void editName(String name){}
+    public void editCredits(int credits){}
+    
+}
