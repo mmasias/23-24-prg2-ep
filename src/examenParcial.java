3c8daf75098189a204d577d9ff2f1b7d6e0dfa8c
nicolas.lopez <nicolas.lopez@alumnos.uneatlantico.es>
2024-04-16 11:02:11 +0200

    Clase Teacher

---
+++ src/examenParcial.java
@@ -0,0 +1,9 @@
+class Teacher {
+
+    public Teacher (String name, String id){
+
+        Teacher mMasias = new Teacher("Manuel Masías", "666999XY");
+        Teacher lAlas = new Teacher("Loyda Alas", "433452A");
+    }
+
+}
